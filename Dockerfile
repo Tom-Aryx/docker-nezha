0726c3a5499c1ec08969ba8b8773f9056028b2eb
Taryx <taryx@proton.me>
2025-06-07 08:57:24 +0000

    alpine

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,21 @@
-FROM debian
+FROM alpine:3.22
 
-WORKDIR /dashboard
+RUN apk add curl git grep iproute2 openrc sed sqlite supervisor unzip util-linux wget && \
+    apk cache clean && \
+    rm -rf /var/cache/apk/* && \
+    mkdir -p /run/openrc && touch /run/openrc/softlevel && \
+    mkdir -p /app/nezha/data && cd /app/nezha && \
+    wget -q https://github.com/nezhahq/nezha/releases/download/$(curl -s https://api.github.com/repos/nezhahq/nezha/releases | grep -m 1 -oP '"tag_name":\s*"\K[^"]+')/dashboard-linux-amd64.zip && \
+    unzip dashboard-linux-amd64.zip && chmod +x dashboard-linux-amd64 && rm dashboard-linux-amd64.zip && \
+    mkdir -p /app/nezha-agent && cd /app/nezha-agent && \
+    wget -q https://github.com/nezhahq/agent/releases/download/$(curl -s https://api.github.com/repos/nezhahq/agent/releases | grep -m 1 -oP '"tag_name":\s*"\K[^"]+')/nezha-agent_linux_amd64.zip && \
+    unzip nezha-agent_linux_amd64.zip && chmod +x nezha-agent && rm nezha-agent_linux_amd64.zip
 
-RUN apt-get update && \
-    apt-get install -y cron curl git iproute2 openssl sed sqlite3 supervisor unzip uuid-runtime wget && \
-    apt-get clean && \
-    rm -rf /var/lib/apt/lists/*
+COPY ./app /app
 
-COPY . .
+RUN chmod +x /app/entrypoint.sh
 
-RUN chmod +x *.sh && ./download.sh
+EXPOSE 8080
 
-ENTRYPOINT ["./entrypoint.sh"]
+CMD [""]
+ENTRYPOINT ["/app/entrypoint.sh"]
---
+++ app/entrypoint.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env sh
+
+DIR_APP="/app"
+DIR_NEZHA="/app/nezha"
+DIR_AGENT="/app/nezha-agent"
+
+DIR_CONFIG="/app/config"
+DIR_SCRIPTS="/app/scripts"
+
+JWT_SECRET=${JWT_SECRET:-"$(openssl rand -base64 48 | sed 's/[\+\/]/q/g')"}
+AGENT_SECRET=${AGENT_SECRET:-"$(openssl rand -base64 24 | sed 's/[\+\/]/q/g')"}
+AGENT_UUID=${AGENT_UUID:-"$(uuidgen)"}
+# NEZHA_DOMAIN
+# GITHUB_REPO
+# AUTO_RESTORE
+
+# first run
+if [ ! -s /etc/supervisor.d/apps.ini ]; then
+    ## ========== permission ==========
+    chmod +x ${DIR_NEZHA}/dashboard-linux-amd64 ${DIR_AGENT}/nezha-agent ${DIR_SCRIPTS}/*.sh
+    ## ========== nezha ==========
+    # replace
+    sed -e "s#-jwt-secret-key-#$JWT_SECRET#" \
+        -e "s#-agent-secret-key-#$AGENT_SECRET#" \
+        -e "s#-install-host-#$NEZHA_DOMAIN#" \
+        -i ${DIR_NEZHA}/config.yaml
+    NEZHA_CMD="${DIR_NEZHA}/dashboard-linux-amd64 -c ${DIR_NEZHA}/config.yaml -db ${DIR_NEZHA}/sqlite.db"
+    ## ========== nezha-agent ==========
+    # replace
+    sed -e "s#-uuid-#$AGENT_UUID#" \
+        -e "s#-agent-secret-key-#$AGENT_SECRET#" \
+        -i ${DIR_AGENT}/config.yaml
+    AGENT_CMD="${DIR_AGENT}/nezha-agent -c ${DIR_AGENT}/config.yaml"
+    ## ========== supervisor ==========
+    # copy
+    mkdir -p /etc/supervisor.d && cp ${DIR_APP}/apps.ini /etc/supervisor.d/apps.ini
+    # replace
+    sed -e "s#-nezha-cmd-#$NEZHA_CMD#g" \
+        -e "s#-agent-cmd-#$AGENT_CMD#g" \
+        -i /etc/supervisor.d/apps.ini
+fi
+
+if [ ! -z "$AUTO_RESTORE" ]; then
+    ${DIR_SCRIPTS}/restore.sh "$GITHUB_REPO"
+fi
+
+# RUN supervisor
+supervisord -c /etc/supervisord.conf
